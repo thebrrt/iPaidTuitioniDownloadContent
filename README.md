2a13cb863fcf41ac7ee647c86d5553ce2bd38115
Albert ML <albert@mindcontainer.com>
2022-02-14 17:53:34 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # iPaidTuitioniDownloadContent
 Python script that takes screenshots of my full stack web dev course for future reference
+
+Built with Selenium
---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+from PIL import Image
+from Screenshot import Screenshot_clipping
+
+driver = webdriver.Chrome(executable_path='resources\chromedriver.exe')
